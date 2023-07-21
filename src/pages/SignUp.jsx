b3f5aa113bcb339bfdd92b7d473b6c293ad629ad
kinyichukwu <kinyichukwuose@gmail.com>
2023-07-21 16:33:49 +0100

    ...

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const SignUp = () => {
   return (
-    <div className="flex min-h-full">
+    <div className="flex min-h-full ">
       <div className="flex-1 hidden lg:flex bg-[#121228] justify-center  flex-col">
         <div className="pl-[5.4%] pr-[4.6%] mb-28 flex items-center">
           <img src={Logo} alt="" className="" />
@@ -54,14 +54,28 @@ const SignUp = () => {
           </Link>
         </p>
 
-
-        
-
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm lg:max-w-[80%] ">
           <form className="space-y-6" action="#" method="POST">
             <div>
               <label className="block text-sm leading-6 text-start text-gray-600">
                 First name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-3 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#00d8709c] sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+
+            <div>
+              <label className="block text-sm leading-6 text-start text-gray-600">
+                Last name
               </label>
               <div className="mt-2">
                 <input
